b7fcbcdcc2ea11efa176c83c2ebfe6bcb7339b98
Aliaksandr Pashkevich <sashabull@mail.ru>
2021-04-08 14:02:21 +0300

    add sort on page & moddify buttons

--- notes.js
+++ notes.js
@@ -1,30 +1,39 @@
-let activeBtn = document.querySelector('#active');
-let allBtn = document.querySelector('#all');
-let doneBtn = document.querySelector('#done');
+const allBtn = document.querySelector('#all'); // раздел all
+const activeBtn = document.querySelector('#active');
+const doneBtn = document.querySelector('#done');
 let progressBar = document.querySelector('.sort__progress-bar')
 const allButtons = document.querySelectorAll('.sort__bar')
+const doneNotes = document.querySelectorAll('.output__note--done') // выполненные заменки
+const activeNotes1 = document.querySelectorAll('.output__note--basic') // активные заметки
+const activeNotes2 = document.querySelectorAll('.output__note--important') // активные заметки
 
+function start () { // дефолтная сортировка all
+    allBtn.classList.toggle('sort--selected-bar')
+}
+
+start(); // дефолтная сортировка all
+
+function sort (event) { // сортировщик
+    if (event.target === activeBtn) { // если активна вкладка active
+        doneNotes.forEach(DoneNotes => DoneNotes.style.display = 'none')
+        activeNotes1.forEach(ActiveNotes1 => ActiveNotes1.style.display = 'block')
+        activeNotes2.forEach(ActiveNotes2 => ActiveNotes2.style.display = 'block')
+    }
+    else if (event.target === doneBtn) {
+        activeNotes1.forEach(ActiveNotes1 => ActiveNotes1.style.display = 'none')
+        activeNotes2.forEach(ActiveNotes2 => ActiveNotes2.style.display = 'none')
+        doneNotes.forEach(DoneNotes => DoneNotes.style.display = 'block')
+    }
+    else if (event.target === allBtn) {
+        doneNotes.forEach(DoneNotes => DoneNotes.style.display = 'block')
+        activeNotes1.forEach(ActiveNotes1 => ActiveNotes1.style.display = 'block')
+        activeNotes2.forEach(ActiveNotes2 => ActiveNotes2.style.display = 'block')
+
+    }
+}
 
 progressBar.addEventListener('click', function (event) { // смена состояния кнопок фильтра
 allButtons.forEach(n => n.classList.remove('sort--selected-bar')) // очищаю класс перед запуском
     event.target.classList.toggle('sort--selected-bar')
-
+    sort(event)
 })
-
-
-
-
-
-
-
-function parseClassList (element) { // проверка на наличие класса
-    let x = element.previousElementSibling.classList.contains('sort--selected-bar')
-    let y = element.nextElementSibling.classList.contains('sort--selected-bar')
-    if (x) {
-        element.previousElementSibling.classList.remove('sort--selected-bar')
-    }
-    else if (y) {
-        element.nextElementSibling.classList.remove('sort--selected-bar')
-    }
-}
-
